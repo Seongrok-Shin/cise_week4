c7502ba9f01fea639753fceb13818e6ca1189613
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2023-08-18 03:03:21 +1200

    (ADD)new pages that called index.tsx and new.tsx in src/pages/articles ,and _app.tsx and index.tsx in pages folder

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,8 @@
+//This will be shown on the home page of the website which is main page of the website
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+    </div>
+  );
+}
