8a8aa70b330879b11959d91d621d262ac53912be
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2023-08-18 03:40:20 +1200

    (Update) Comment out some files, (ADD) Needed dependencies were installed

--- frontend/src/pages/articles/index.tsx
+++ frontend/src/pages/articles/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps, NextPage } from "next";
 import SortableTable from "../../components/table/SortableTable";
-// import data from "../../utils/dummydata.json";
+import data from "../../utils/dummydata.json";
 
 interface ArticlesInterface {
   id: string;
@@ -37,24 +37,24 @@ const Articles: NextPage<ArticlesProps> = ({ articles }) => {
   );
 };
 
-// export const getStaticProps: GetStaticProps<ArticlesProps> = async (_) => {
-//   // Map the data to ensure all articles have consistent property names
-//   const articles = data.articles.map((article) => ({
-//     id: article.id ?? article._id,
-//     title: article.title,
-//     authors: article.authors,
-//     source: article.source,
-//     pubyear: article.pubyear,
-//     doi: article.doi,
-//     claim: article.claim,
-//     evidence: article.evidence,
-//   }));
+export const getStaticProps: GetStaticProps<ArticlesProps> = async (_) => {
+  // Map the data to ensure all articles have consistent property names
+  const articles = data.articles.map((article) => ({
+    id: article.id ?? article._id,
+    title: article.title,
+    authors: article.authors,
+    source: article.source,
+    pubyear: article.pubyear,
+    doi: article.doi,
+    claim: article.claim,
+    evidence: article.evidence,
+  }));
 
-//   return {
-//     props: {
-//       articles,
-//     },
-//   };
-// };
+  return {
+    props: {
+      articles,
+    },
+  };
+};
 
 export default Articles;
